6622fd352ae101688f9f447584153a5f25a75db8
khang <khang.td194591@sis.hust.edu.vn>
2023-02-04 16:55:28 +0700

    ✨Add authentication features✨

--- db/migrate/20230204090010_create_movies.rb
+++ db/migrate/20230204090010_create_movies.rb
@@ -1,6 +1,7 @@
 class CreateMovies < ActiveRecord::Migration[6.1]
   def change
     create_table :movies do |t|
+      t.string :title
       t.text :short_description
       t.string :trailer_url
       t.text :storyline
