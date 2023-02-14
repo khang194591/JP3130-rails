b3ac568b47659a93961525b35b8885c5fb8f4b0e
khang <khang.td194591@sis.hust.edu.vn>
2023-02-14 12:35:36 +0700

    💄update ui💄

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,22 +1,21 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
   content: [
-    './public/*.html',
-    './app/helpers/**/*.rb',
-    './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}'
+    "./public/*.html",
+    "./app/helpers/**/*.rb",
+    "./app/javascript/**/*.js",
+    "./app/views/**/*.{erb,haml,html,slim}",
   ],
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Noto Sans JP', ...defaultTheme.fontFamily.sans],
+        sans: ["Noto Sans JP", ...defaultTheme.fontFamily.sans],
       },
     },
   },
-  plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/aspect-ratio'),
-    require('@tailwindcss/typography'),
-  ]
-}
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["cmyk"],
+  },
+};
